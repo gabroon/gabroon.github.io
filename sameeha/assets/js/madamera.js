0910b0dac4fb088e5e4de89a670ed350e65046fa
gabroon <moh_webdev@hotmail.com>
2017-08-08 11:06:12 +0100

    Update madamera.js

--- sameeha/assets/js/madamera.js
+++ sameeha/assets/js/madamera.js
@@ -75,7 +75,7 @@ $('#convert_madamera').click(function(){
 		var arrayOfSentences = rawText.split(';');
 	//if array not empty 
 	for (var i = 0 ; i <= arrayOfSentences.length - 1; i++) {
-		madmeraOutPut = madmeraOutPut + "<in_seg id=\"SENT"+i+"\">" +arrayOfSentences[i] +"</in_seg> "
+		madmeraOutPut = madmeraOutPut + "<in_seg id=\"SENT"+i+"\">" +arrayOfSentences[i] +"</in_seg> \n"
 		
 	};
 
